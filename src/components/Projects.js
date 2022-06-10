41eec144cc4facd5d1ed5c11982a1c676c4f55d5
Victoria Diaz <diazmariavictoria94@gmail.com>
2022-06-09 22:43:28 -0300

    Agrego links en proyectos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,16 @@
+import "../styles/_Projects.scss";
+
 const Projects = () => {
-	return <div>Projects</div>;
+	return (
+		<>
+			<h1 className="title-projects">Projects</h1>
+			<div className="box-projects">
+				<a href="https://diazvic-buscador-de-peliculas.netlify.app/">
+					Buscador de Peliculas
+				</a>
+			</div>
+		</>
+	);
 };
 
 export default Projects;
